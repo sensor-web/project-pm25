edc1f1652268fc268cde409009b0cd87b185e416
Mark Liang <big.head.cool@gmail.com>
2016-07-14 12:57:41 +0800

    Upload webpage visual

--- public/js/station.js
+++ public/js/station.js
@@ -26,13 +26,13 @@ function getDAQIStatusText(index) {
   if (!isNumeric(index) || index < 0) {
     return '資料錯誤';
   } else if (index <= 35) {
-    return '建議：正常戶外活動。';
+    return '空氣品質指標良好，可以正常戶外活動。';
   } else if (index <= 53) {
-    return '建議：有心臟、呼吸道及心血管疾病的成人與孩童感受到癥狀時，應考慮減少體力消耗，特別是減少戶外活動。';
+    return '空氣品質指標適中，有心臟、呼吸道及心血管疾病的成人與孩童應考慮減少戶外活動。';
   } else if (index <= 70) {
-    return '建議：<ol><li>有心臟、呼吸道及心血管疾病的成人與孩童，應減少體力消耗，特別是減少戶外活動。</li><li>老年人應減少體力消耗。</li><li>3. 具有氣喘的人可能需增加使用吸入劑的頻率。</li></ol>';
+    return '空氣品質指標危險，任何人如果有不適，如眼痛，咳嗽或喉嚨痛等，應該考慮減少戶外活動。';
   } else {
-    return '建議：<ol><li>有心臟、呼吸道及心血管疾病的成人與孩童，以及老年人應避免體力消耗，特別是避免戶外活動。</li><li>具有氣喘的人可能需增加使用吸入劑的頻率。</li></ol>';
+    return '空氣品質指標高危險，任何人如果有不適，如眼痛，咳嗽或喉嚨痛等，應減少體力消耗，特別是減少戶外活動。';
   }
 }
 
@@ -42,7 +42,7 @@ if (typeof did !== 'undefined' && did != '') {
     var feeds = $.parseJSON(data)['feeds'];
     var latest = feeds[feeds.length - 1];
     var pm25 = latest['PM2_5'];
-    $('#latest-pm25').text(pm25).attr('class', getDAQIStatus(pm25));
+    $('#latest-pm25').text(parseInt(pm25)).attr('class', getDAQIStatus(pm25));
     moment.locale('zh-tw');
     $('#latest-status').text(getDAQIStatusText(pm25));
     $('#latest-time').text(moment(latest['timestamp'], ISO_FORMAT).fromNow());
