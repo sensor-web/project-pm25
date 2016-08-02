00981e6ace6b1616a09c407fcff295ac9b7784b0
Eddie Lin <yshlin@gmail.com>
2016-08-02 10:35:59 +0800

    Added script to generate sitemap and find nearby stations.

---
+++ data/genSitemap.js
@@ -0,0 +1,20 @@
+var sm = require('sitemap');
+var fs = require('fs');
+var stations = require('./stations.json');
+var urls = [];
+for (var slug in stations) {
+	var url = {
+		url: 'https://sensorweb.io/pm25/station/'+slug+'/',
+		changefreq: 'hourly',
+		priority: 0.8
+	};
+	urls.push(url);
+}
+
+var sitemap = sm.createSitemap({
+    hostname: 'http://sensorweb.com',
+    cacheTime: 3600000,  //600 sec (10 min) cache purge period 
+    urls: urls
+});
+ 
+fs.writeFileSync("./sitemap.xml", sitemap.toString());
