7313c8cd730abb8f5f5264b2983ee95c65fe766e
Eddie Lin <yshlin@gmail.com>
2016-08-11 15:29:28 +0800

    Added link for PM2.5 description.

--- public/js/station.js
+++ public/js/station.js
@@ -1,7 +1,7 @@
 moment.locale('zh-tw');
 var $pm25 = $('#latest-pm25');
 $pm25.attr('class', getDAQIStatus(Number.parseInt($pm25.text())));
-$('#latest-status').text(getDAQIStatusText(Number.parseInt($pm25.text())));
+$('#latest-status').html(getDAQIStatusText(Number.parseInt($pm25.text())));
 
 $('.concentration-value').each(function() {
   var $this = $(this);
@@ -68,12 +68,12 @@ function getDAQIStatusText(index) {
   if (!isNumeric(index) || index < 0) {
     return '資料錯誤';
   } else if (index <= 35) {
-    return '空氣品質指標良好，可以正常戶外活動。';
+    return '<span class="marked">空氣品質指標良好</span>，可以正常戶外活動。';
   } else if (index <= 53) {
-    return '空氣品質指標適中，有心臟、呼吸道及心血管疾病的成人與孩童應考慮減少戶外活動。';
+    return '<span class="marked">空氣品質指標適中</span>，有心臟、呼吸道及心血管疾病的成人與孩童應考慮減少戶外活動。';
   } else if (index <= 70) {
-    return '空氣品質指標危險，任何人如果有不適，如眼痛，咳嗽或喉嚨痛等，應該考慮減少戶外活動。';
+    return '<span class="marked">空氣品質指標危險</span>，任何人如果有不適，如眼痛，咳嗽或喉嚨痛等，應該考慮減少戶外活動。';
   } else {
-    return '空氣品質指標高危險，任何人如果有不適，如眼痛，咳嗽或喉嚨痛等，應減少體力消耗，特別是減少戶外活動。';
+    return '<span class="marked">空氣品質指標高危險</span>，任何人如果有不適，如眼痛，咳嗽或喉嚨痛等，應減少體力消耗，特別是減少戶外活動。';
   }
 }
