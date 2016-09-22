94af16e3999757e8febb944a8edeaf9e29cebf4c
Eddie Lin <yshlin@gmail.com>
2016-09-22 13:21:18 +0800

    Update SIDs while migrate subscriptions.

--- data/migrateToMailchimp.js
+++ data/migrateToMailchimp.js
@@ -1,7 +1,7 @@
 var config = require('../config');
 var db = require('../lib/db');
 var subscriptions = require('../lib/subscriptions');
-var ctx = null;
+var ctx = {batchSize: config.db_batch_size};
 
 function onSuccess(result) {console.log(result)};
 function onError(error) {console.error(error)};
@@ -9,11 +9,14 @@ function onError(error) {console.error(error)};
 db.connect(config.rethinkdb).then(function (db) {
     subscriptions.setMailchimp(config.mailchimp);
     subscriptions.setDatabase(db);
-    subscriptions.list(ctx, true).then(function (results) {
+    subscriptions.updateSids(ctx).then(function (results) {
+        console.log('updated SIDs');
+        return subscriptions.list(ctx, true);
+    }).then(function (results) {
+        console.log('list all subscriptions');
         return subscriptions.batchSubscribeOnMailchimp(ctx, results);
     }).then(function (result) {
         console.log(result);
         db.disconnect();
     }).catch(onError);
-    //TODO: save sids
 });
