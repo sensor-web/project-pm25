4fa12486baa6bac6994fdd7ff23aa7c1e466117a
Eddie Lin <yshlin@gmail.com>
2016-09-22 13:37:42 +0800

    Merge duplicate subscriptions for migration.

--- data/migrateToMailchimp.js
+++ data/migrateToMailchimp.js
@@ -13,10 +13,43 @@ db.connect(config.rethinkdb).then(function (db) {
         console.log('updated SIDs');
         return subscriptions.list(ctx, true);
     }).then(function (results) {
-        console.log('list all subscriptions');
-        return subscriptions.batchSubscribeOnMailchimp(ctx, results);
+        console.log(results);
+        console.log('merge all subscriptions');
+        var subs = {};
+        var subsQ = [];
+        for (var result of results) {
+            if (undefined != subs[result.email_address]) {
+                subs[result.email_address] = mergeSubscriptions(subs[result.email_address], result);
+            } else {
+                subs[result.email_address] = result;
+            }
+        }
+        for (var i in subs) {
+            subsQ.push(subs[i]);
+        }
+        return subscriptions.batchSubscribeOnMailchimp(ctx, subsQ);
     }).then(function (result) {
+        console.log('imported to mailchimp:');
         console.log(result);
         db.disconnect();
     }).catch(onError);
 });
+
+function mergeSubscriptions(sub1, sub2) {
+    if (undefined != sub2.merge_fields.MYGPS) {
+        sub1.merge_fields.MYGPS = sub2.merge_fields.MYGPS;
+    }
+    if (undefined != sub2.merge_fields.FREQ) {
+        sub1.merge_fields.FREQ = sub2.merge_fields.FREQ;
+    }
+    if (undefined != sub2.merge_fields.WHYJOIN) {
+        sub1.merge_fields.WHYJOIN = sub2.merge_fields.WHYJOIN;
+    }
+    if (undefined != sub2.merge_fields.GETASENSOR) {
+        sub1.merge_fields.GETASENSOR = sub2.merge_fields.GETASENSOR;
+    }
+    if (undefined != sub2.interests) {
+        sub1.interests = sub2.interests;
+    }
+    return sub1;
+}
